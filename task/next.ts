3d59be837aa3525130021b0df8958e9e00cf6cd2
kmatsui058 <kmatsui058@gmail.com>
2021-01-04 12:22:14 +0900

    next taskのパス修正

--- task/next.ts
+++ task/next.ts
@@ -1,19 +1,20 @@
 import _fs = require('fs');
 const fs = _fs.promises
 import dayjs from 'dayjs'
+import path = require('path');
 
 async function main() {
-    const oldDir = `../old/${dayjs().format('YYYY-MM-DD-hh-mm-ss')}/`
-    const templateDir = "../template/"
-    const srcDir = "../src/"
-    const distDir = "../dist/"
+    const oldDir = path.resolve(__dirname, `../old/${dayjs().format('YYYY-MM-DD-hh-mm-ss')}/`)
+    const templateDir = path.resolve(__dirname, "../template/")
+    const srcDir = path.resolve(__dirname, "../src/")
+    const distDir =path.resolve(__dirname,  "../dist/")
     await fs.mkdir(oldDir)
-    await fs.mkdir(oldDir + "dist/")
-    await fs.copyFile(srcDir + "answer.ts", oldDir + "answer.ts")
-    await fs.copyFile(distDir + "answer.js", oldDir + "dist/answer.js")
-    await fs.copyFile(srcDir + "testdata.json", oldDir + "testdata.json")
-    await fs.copyFile(templateDir + "testdata.json", srcDir + "testdata.json")
-    await fs.copyFile(templateDir + "answer.txt", srcDir + "answer.ts")
+    await fs.mkdir(path.resolve(oldDir, "dist/"))
+    await fs.copyFile(path.resolve(srcDir, "answer.ts"), path.resolve(oldDir, "answer.ts"))
+    await fs.copyFile(path.resolve(distDir, "answer.js"), path.resolve(oldDir, "dist/answer.js"))
+    await fs.copyFile(path.resolve(srcDir, "testdata.json"), path.resolve(oldDir, "testdata.json"))
+    await fs.copyFile(path.resolve(templateDir, "testdata.json"), path.resolve(srcDir, "testdata.json"))
+    await fs.copyFile(path.resolve(templateDir, "answer.txt"), path.resolve(srcDir, "answer.ts"))
 }
 
 main()
